75a2e799805cc592056cc783a8ba2595fa3d2fef
eduardoBalinas <eduardo.balinas.lira@gmail.com>
2021-10-25 19:44:42 -0500

    juego de verdadero falso

---
+++ src/components/Indecision.vue
@@ -0,0 +1,93 @@
+<template>
+    <h1>Indecision</h1>
+    <div class="bg-dark"></div>
+    <div class="indecision-container">
+        <input v-model="question" type="text" placeholder="Hazme una pregunta" name="" id="">
+        <p>Recuerda terminar con un signo de interrogacion (?)</p>
+
+        <div>
+            <h2>{{ question }}</h2>
+            <h1>{{ answer }}</h1>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            question: null,
+            answer: null,
+            isValidQuestion: false
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = "Pensando...";
+            const { answer } = await fetch('https://yesno.wtf/api').then( r => r.json() )
+            this.answer = answer === "yes" ? "SI!" : "NO!";
+        }
+    },
+    watch: {
+        question: function(value) {
+            if(this.question.slice(-1) == "?") {
+                this.isValidQuestion = true;
+                this.getAnswer();
+            }
+            if(this.question == "") {
+                this.answer = ""
+            }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+    .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(22, 16, 16, 0.973);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
+
+</style>
